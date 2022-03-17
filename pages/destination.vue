d646a9ef0d82a0238cc901da5277ee74cd5e8210
mxristova <79445531+mxristova@users.noreply.github.com>
2022-03-17 23:23:33 +0100

    Update destination

--- pages/destination.vue
+++ pages/destination.vue
@@ -1,26 +1,61 @@
 <template>
-  <h1 class="numbered-title"><span>01</span>Pick your Destination</h1>
+  <div class="container flow flex crew">
+    <h1 class="numbered-title"><span>01</span> Pick your Destination</h1>
+    <div id="content" v-if="destination">
+      <img :src="destination['png-image'].guid" />
+      <ul>
+        <li
+          v-for="(item, index) in destinations"
+          :key="item['destination-name']"
+          @click="setDestination(index)"
+        >
+          {{ item['destination-name'] }}
+        </li>
+      </ul>
+      <h1>{{ destination['destination-name']}}</h1>
+      <p>{{ destination.description }}</p>
+      <div>
+        <span> {{ destination.distance }} </span>
+        <span> {{ destination.travel }} </span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
+  data() {
+    return {
       destinations: [],
+      activeDestinationIndex: 3,
     }
   },
   methods: {
-    async loadData(){
+    async loadData() {
       const response = await fetch(
         'https://space.wp.seiwald.hak-stjo.schulwebspace.at/wp-json/wp/v2/destination'
       )
       this.destinations = await response.json()
     },
+    setDestination(index) {
+      this.activeDestinationIndex = index
+    },
   },
-  mounted(){
+  mounted() {
     this.loadData()
     document.body.style.backgroundImage =
-    "url('background-destination-mobile.jpg')"
-  }
+      "url('background-destination-mobile.jpg')"
+  },
+  computed: {
+    destination() {
+      return this.destinations[this.activeDestinationIndex]
+    },
+  },
 }
 </script>
+
+<style scoped>
+.numbered-title {
+  font-size: 16px;
+}
+</style>
